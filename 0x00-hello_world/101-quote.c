705f12612eb5d90da80c68cf2e631abc7bdc024e
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2022-03-11 02:59:41 +0100

    C-Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
+/* main - program startup
+ (*
+ * Description: prints the exact phrase followed by a new line)?
+ * Return: return (0) is the required function signature
+ */
+
+
 #include <stdio.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
+ * main - prints exactly and that piece of art is useful"
  * - Dora Kpr[ar, "2015-10-19",
  * followed by a new line, to the standard error.
  * Return:Always 0 (success)
