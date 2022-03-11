881f08f869f7cbe7e5c1ed759df59113bb7309ad
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2022-03-11 07:21:03 +0100

    C-Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,13 @@
-/* main - program startup
- (*
- * Description: prints the exact phrase followed by a new line)?
- * Return: return (0) is the required function signature
- */
-
-
-#include <stdio.h>
+#include <stdio>
+#include <unistdio>
 
 /**
- * main - prints exactly and that piece of art is useful"
- * - Dora Kpr[ar, "2015-10-19",
- * followed by a new line, to the standard error.
- * Return:Always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-int main(void)
+int main (void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
