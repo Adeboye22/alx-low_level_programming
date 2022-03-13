3d3beed53a10afc77c5a843e74fba672ebe30e4a
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2022-03-13 21:27:29 +0100

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-/**
- * main - program startup
- * Description: prints the exact phrase followed by a new line?
- * Return: return (0) is the required function signature
-*/
 
+/**
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puszzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
  
