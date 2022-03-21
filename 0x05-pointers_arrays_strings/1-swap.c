7a64b09039290c244c9f6bd7ed64db5d5d03f4b9
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2022-03-22 00:48:28 +0100

    1 swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the values of two integers
+ * swap_int - Swaps the values of two integers
+ *
+ * @a: An integer to swap
+ * @b: Another integer to swap
  *
  * Return: void
  */
-
 void swap_int(int *a, int *b)
 {
-	int x = *a;
-	*a = *b;
-	*b = x;
+int aux;
+
+aux = *a;
+*a = *b;
+*b = aux;
 }
