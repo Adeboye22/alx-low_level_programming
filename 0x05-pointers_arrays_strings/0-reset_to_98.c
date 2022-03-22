daf60eaf9dc0013f7c4488a2d2750c640f295fda
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2022-03-22 01:17:49 +0100

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer and updates the value it points to
- *
+ * reset_to_98 - takes a pointer and updates the value it point to
+ * @n: pointer
  * Return: void
  */
 
@@ -10,3 +10,4 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
